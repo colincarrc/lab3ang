7ba8bce493e794ffdc2021462a635eb745aac4cf
COLIN CARR - STUDENT <G00357742@gmit.ie>
2019-02-06 17:14:46 +0000

    first commit

---
+++ src/app/b/b.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-b',
+  templateUrl: './b.component.html',
+  styleUrls: ['./b.component.css']
+})
+export class BComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
